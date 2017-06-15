2cd13d657242a5e9f7fd9ac1cf767afa5aa2d6af
uberbryn <hi@bryn.io>
2017-06-15 14:38:25 -0700

    misc updates over time

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,7 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias spc='cd ~/code/spectrum'
+
+alias fixssh='ssh-add -K ~/.ssh/id_rsa'
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -71,7 +71,7 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'%{$fg_bold[yellow]%}\nsystem%{$reset_color%} in $(directory_name) $(git_dirty)$(need_push)\n→ '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
